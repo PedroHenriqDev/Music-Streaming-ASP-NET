10ae68cd993c180e4f386d2d7692ba9b18286b4a
Pedro Henirique <heroperde@gmail.com>
2024-03-22 22:56:26 -0300

    Created Project

---
+++ MusicWeave/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MusicWeave.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
