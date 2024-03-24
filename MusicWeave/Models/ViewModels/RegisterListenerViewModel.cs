466c4bcc23497f0d679a2b72b97baf4f85cd888c
Pedro Henirique <heroperde@gmail.com>
2024-03-24 15:00:55 -0300

    Added attribute to restrict users under 10 years old

--- MusicWeave/Models/ViewModels/RegisterListenerViewModel.cs
+++ MusicWeave/Models/ViewModels/RegisterListenerViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using MusicWeave.Models.ConcreteClasses;
 using MusicWeave.Models.Interfaces;
+using MusicWeave.Attributes;
 using System.ComponentModel.DataAnnotations;
 
 namespace MusicWeave.Models.ViewModels
@@ -16,6 +17,7 @@ namespace MusicWeave.Models.ViewModels
         public string Email { get; set; }
 
         [Required]
+        [Age(ErrorMessage = "It's mandatory be at least 10 years old!")]
         public DateTime BirthDate { get; set; }
 
         public string? PhoneNumber { get; set; }
