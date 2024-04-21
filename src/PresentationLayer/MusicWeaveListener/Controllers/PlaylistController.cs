0e72ce28fb6b475c196c357fbd5edad2c9399c44
Pedro Henirique <heroperde@gmail.com>
2024-04-21 18:36:10 -0300

    Implemented view for adding a new playlist

---
+++ src/PresentationLayer/MusicWeaveListener/Controllers/PlaylistController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MusicWeaveListener.Controllers
+{
+    public class PlaylistController : Controller
+    {
+        public IActionResult Playlists()
+        {
+            return View();
+        }
+
+        public IActionResult AddPlaylist() 
+        {
+            return View();
+        }
+    }
+}
