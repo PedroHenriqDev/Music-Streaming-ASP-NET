d0d2ea179cbf8d65618dbc14b8bc3b2ad9a44c6f
Pedro Henirique <heroperde@gmail.com>
2024-04-28 23:34:42 -0300

    Added a search-related controller

---
+++ src/PresentationLayer/MusicWeaveListener/Controllers/SearchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MusicWeaveListener.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult SearchMusicToPlaylist(string query)
+        {
+            return View();
+        }
+    }
+}
